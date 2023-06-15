7e1258f7e070dfa4406474931024d6734a81f97e
Murilo Rodrigues <murilorodrigues2022@outlook.com>
2023-06-15 11:57:35 -0300

    1005 - Média 1

---
+++ beecrowd/iniciante/bee1005/Program.cs
@@ -0,0 +1,7 @@
+﻿const double PESO1 = 3.5;
+const double PESO2 = 7.5;
+double A = double.Parse(System.Console.ReadLine().Trim());
+double B = double.Parse(System.Console.ReadLine().Trim());
+double Media = ((A * PESO1) + (B * PESO2)) / (PESO1 + PESO2);
+Media = Math.Round(Media, 5);
+System.Console.WriteLine($"MEDIA = {Media.ToString("#0.00000")}");
