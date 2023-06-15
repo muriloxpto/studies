101fff1de02137d9036106ec614fb65388dde2a8
Murilo Rodrigues <murilorodrigues2022@outlook.com>
2023-06-15 14:28:32 -0300

    1168 - LED

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Projetos criados durante o estudo de alguma ferramenta ou técnica
 ## Estudando C#
 [1004 - Produto Simples](https://github.com/muriloxpto/estudos/tree/453c490049a1819599528adab59415bb4afe2faf/beecrowd/iniciante/bee1004)<br/>
 [1005 - Média 1](https://github.com/muriloxpto/estudos/tree/bf6ed7ef12593ab44d1147586ff171846bb33714/beecrowd/iniciante/bee1005)
+[1168 - LED]()
---
+++ beecrowd/strings/bee1168/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+//quantidade de LEDs necessários para formar o indice correspondente
+int[] leds = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
+int qtdTestes = Int32.Parse(System.Console.ReadLine().Trim());
+for (int caso = 1; caso <= qtdTestes; caso++)
+{
+    int qtdLEDs = 0;
+    string entrada = Convert.ToString(Console.ReadLine().Trim());
+    for (int digito = 0; digito < entrada.Length; digito++)
+    {
+        var led = Convert.ToInt32(entrada[digito]) - 48;
+        qtdLEDs += leds[led];
+    }
+    System.Console.WriteLine($"{qtdLEDs} leds");
+}
