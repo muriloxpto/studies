453c490049a1819599528adab59415bb4afe2faf
Murilo Oliveira Rodrigues <104870624+muriloxpto@users.noreply.github.com>
2023-06-15 11:28:56 -0300

    1004 - Produto Simples

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # estudos
 Projetos criados durante o estudo de alguma ferramenta ou técnica
+
+# Beecrowd
+[1004 - Produto Simples]()
---
+++ beecrowd/iniciante/bee1004/Program.cs
@@ -0,0 +1,4 @@
+﻿int A = Int32.Parse(System.Console.ReadLine().Trim());
+int B = Int32.Parse(System.Console.ReadLine().Trim());
+int prod = A * B;
+Console.WriteLine($"PROD = {prod}");
