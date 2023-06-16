65aa371366621d2f18fb795e74433f1f8b63722a
Murilo Rodrigues <murilorodrigues2022@outlook.com>
2023-06-16 08:53:00 -0300

    1024 - Criptografia

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# estudos
+<h1>estudos</h1>
 Projetos criados durante o estudo de alguma ferramenta ou técnica
 
-# Beecrowd
-## Estudando C#
-[1004 - Produto Simples](https://github.com/muriloxpto/estudos/tree/453c490049a1819599528adab59415bb4afe2faf/beecrowd/iniciante/bee1004)<br/>
-[1005 - Média 1](https://github.com/muriloxpto/estudos/tree/bf6ed7ef12593ab44d1147586ff171846bb33714/beecrowd/iniciante/bee1005)<br/>
+<h2>Estudando C#</h2>
+1004 - Produto Simples | [Github](https://github.com/muriloxpto/estudos/tree/453c490049a1819599528adab59415bb4afe2faf/beecrowd/iniciante/bee1004)
+[Beecrowd](https://www.beecrowd.com.br/judge/pt/problems/view/1004)<br/>
+[1005 - Média 1](https://github.com/muriloxpto/estudos/tree/bf6ed7ef12593ab44d1147586ff171846bb33714/beecrowd/iniciante/bee1005)
+[Beecrowd](https://www.beecrowd.com.br/judge/pt/problems/view/1005)<br/>
 [1168 - LED](https://github.com/muriloxpto/estudos/tree/101fff1de02137d9036106ec614fb65388dde2a8/beecrowd/strings/bee1168)
+[Beecrowd](https://www.beecrowd.com.br/judge/pt/problems/view/1168)<br/>
+1024 - Criptografia | [Github]() [Beecrowd](https://www.beecrowd.com.br/judge/pt/problems/view/1024)
---
+++ beecrowd/strings/bee1024/Program.cs
@@ -0,0 +1,38 @@
+﻿int casos = Int32.Parse(System.Console.ReadLine().Trim());
+for (int caso = 1; caso <= casos; caso++)
+{
+    string mensagem = System.Console.ReadLine().Trim();
+    char[] etapa1 = mensagem.ToCharArray();
+    int qtdChars = mensagem.Length;
+    //----- 1° Etapa -----
+    //Letras minúsculas e maiúsculas devem ser deslocadas 3 posições para a direita
+    for (int index = 0; index < qtdChars; index++)
+    {
+        int asciiCode = Convert.ToInt32(mensagem[index]);
+        //Verificando se é uma letra
+        if ((asciiCode >= 65 && asciiCode <= 90) || (asciiCode >= 97 && asciiCode <= 122))
+        {
+            asciiCode += 3;
+            etapa1[index] = Convert.ToChar(asciiCode);
+        }
+    }
+    //----- 2° Etapa -----
+    //Inverter a ordem da mensagem
+    char[] etapa2 = new char[qtdChars];
+    for (int index = 0; index < qtdChars; index++)
+    {
+        //Indo do fim da fila para o inicio
+        int posicao = (qtdChars-1) - index;
+        etapa2[posicao] = etapa1[index];
+    }
+    //----- 3° Etapa -----
+    //Todo e qualquer caractere a partir da metade em diante (truncada) devem ser deslocados uma posição para a esquerda na tabela ASCII.
+    char[] etapa3 = etapa2;
+    int metade = qtdChars/2;
+    for (int index = metade; index < qtdChars; index++)
+    {
+        int asciiCode = Convert.ToInt32(etapa3[index]) - 1;
+        etapa3[index] = Convert.ToChar(asciiCode);
+    }
+    System.Console.WriteLine(etapa3);
+}
